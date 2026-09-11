0e9ba21660541f87313a937b22e37b95a24e7b99
w14769259154-alt <w14769259154@gmail.com>
2026-09-11 13:07:44 +0800

    Use system autoreconf to generate configure

--- audio/build_snd.sh
+++ audio/build_snd.sh
@@ -82,7 +82,7 @@ cd "$WORK"
 wget -q "$ALSA_LIB_URL" -O alsa-lib.tgz
 tar -xzf alsa-lib.tgz
 cd alsa-lib-${ALSA_VER}
-autoreconf -fi                 # GitHub 源码不含 configure, 需先生成(autoconf 项目)
+/usr/bin/autoreconf -fi         # GitHub 源码不含 configure; 用系统 autoconf(PATH 里工具链自带的是坏脚本)
 ./configure --host="$CROSS" --prefix="$STAGE/usr" \
   --enable-static --disable-shared --with-pic >/dev/null
 make -j"$(nproc)" >/dev/null && make install >/dev/null
@@ -92,7 +92,7 @@ cd "$WORK"
 wget -q "$ALSA_UTILS_URL" -O alsa-utils.tgz
 tar -xzf alsa-utils.tgz
 cd alsa-utils-${ALSA_VER}
-autoreconf -fi                 # 同上, 源码不含 configure
+/usr/bin/autoreconf -fi         # 同上, 用系统 autoconf 生成 configure
 export PKG_CONFIG_PATH="$STAGE/usr/lib/pkgconfig"
 ./configure --host="$CROSS" --prefix="$STAGE/usr" \
   --disable-alsaconf --disable-alsactl --disable-alsaloop \
